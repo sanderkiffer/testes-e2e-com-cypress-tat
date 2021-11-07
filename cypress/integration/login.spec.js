8b809d4fc15e921022a05d39fcd14ae84675d801
sanderkiffer <kiffer9915@gmail.com>
2021-11-07 19:12:29 -0300

    .

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,10 +2,10 @@ it.only('successfully logs in', () => {
   cy.intercept('GET', '**/notes').as('getNotes')
 
   cy.login(
-    '83d1326d-dd9d-47b9-81e8-ec72f5cb0f45@fp9klczf.mailosaur.net',
-    's3Cre7P@sSw0rd',
+    Cypress.env('USER_EMAIL'),
+    Cypress.env('USER_PASSWORD'),
     { cacheSession: false }
   )
 
-  cy.wait('@getNotes', {timeout: 30000})
+  cy.wait('@getNotes', {timeout: 20000})
 })
