31fcb7589034be0e3ba63b7cdfd4d3dc2d44fb70
sanderkiffer <kiffer9915@gmail.com>
2021-11-07 16:45:42 -0300

    .

--- cypress/integration/authenticatedScenarios.spec.js
+++ cypress/integration/authenticatedScenarios.spec.js
@@ -35,7 +35,7 @@ describe('Scenarios where authentication is a pre-requirement', () => {
   it('logs out', () => {
     cy.visit('/')
     cy.wait('@getNotes', {timeout: 15000})
-    //teste1
+    //teste2
     if (Cypress.config('viewportWidth') < Cypress.env('viewportWidthBreakpoint')) {
       cy.get('.navbar-toggle.collapsed')
         .should('be.visible')
